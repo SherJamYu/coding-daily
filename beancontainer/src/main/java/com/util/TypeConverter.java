db6cac020a65eca44a10575ce4a76d20940a82fa
SherJamYu <zhangyu.xzy@aliyun.com>
2015-11-27 13:12:23 +0800

    add maven project beancontianer

---
+++ beancontainer/src/main/java/com/util/TypeConverter.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.util;
+
+/**
+ * 
+ * @author <a href="zhangyu.xzy@aliyun.com">薛羽</a>
+ * @version 1.1 2015年11月27日
+ * @since 1.1
+ */
+public class TypeConverter {
+    public static Integer convertToInteger(String before) {
+        return Integer.parseInt(before);
+    }
+
+    public static Integer convertToInt(String before) {
+        return Integer.parseInt(before);
+    }
+}
